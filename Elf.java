90b17832ae8262ac1a42fdc26787b595467cb89b
AnKukl <131239026+AnKukl@users.noreply.github.com>
2023-05-13 17:59:34 +0200

    Add files via upload

---
+++ Elf.java
@@ -0,0 +1,52 @@
+package HW2.Game.hero;
+
+public class Elf {
+
+
+        private int age;
+        private String adress;
+        private String name ;
+        private int wieght;
+        private int high;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHigh() {
+        return high;
+    }
+
+    public void setHigh(int high) {
+        this.high = high;
+    }
+
+    public int getWieght() {
+        return wieght;
+    }
+
+    public void setWieght(int wieght) {
+        this.wieght = wieght;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
+
+
+
---
+++ Java.java
@@ -0,0 +1,21 @@
+package HW2.Game.hero;
+
+public class Java {
+    public static void main(String[] args) {
+    String a = "I study basic Java!";
+    char b = a.charAt(18);
+    System.out.println("18 symbol is:" + b);
+    System.out.println(a.contains("Java"));
+
+    System.out.println(a.replace('a', 'o'));
+    System.out.println(a.toUpperCase());
+    System.out.println(a.toLowerCase());
+    System.out.println(a.substring(0, 13));
+
+
+
+
+
+
+}
+}
